f38db1f3c8ebfea479f014145ece2c591ec1abbd
Ricardo Boss <contact@ricardoboss.de>
2022-01-25 12:17:18 +0100

    Don't use relative path

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Elephox\Core\Core;
 use Elephox\Docs\App;
 
-require_once '../vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $core = Core::create();
 $core->registerApp(App::class);
