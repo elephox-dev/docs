574858afdc32da0c958926185f9f816fae5dc813
Ricardo Boss <contact@ricardoboss.de>
2022-04-10 23:04:23 +0200

    Updated framework usages

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 declare(strict_types=1);
 
-$core = require dirname(__DIR__) . '/bootstrap.php';
-$core->handleGlobal();
+define('APP_ROOT', dirname(__DIR__));
+require_once APP_ROOT . '/vendor/autoload.php';
+
+use Elephox\Docs\ContentFiles;
+use Elephox\Docs\Routes;
+use Elephox\Web\Routing\RequestRouter;
+use Elephox\Web\WebApplicationBuilder;
+
+$builder = WebApplicationBuilder::create();
+$builder->addWhoops();
+$builder->setRequestRouterEndpoint();
+$builder->service(RequestRouter::class)->loadFromClass(Routes::class);
+$builder->services->addSingleton(ContentFiles::class);
+$app = $builder->build();
+$app->run();
---
+++ src/Routes.php
@@ -0,0 +1,86 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Docs;
+
+use Elephox\Files\Path;
+use Elephox\Http\Contract\Request;
+use Elephox\Http\Contract\ResponseBuilder;
+use Elephox\Http\Response;
+use Elephox\Http\ResponseCode;
+use Elephox\Mimey\MimeType;
+use Elephox\Web\Routing\Attribute\Controller;
+use Elephox\Web\Routing\Attribute\Http\Any;
+use Elephox\Web\Routing\Attribute\Http\Get;
+
+#[Controller('/')]
+class Routes
+{
+    #[Any('regex:(?<url>.*)')]
+    public function handleAny(Request $request, PageRenderer $pageRenderer): ResponseBuilder
+    {
+        $url = ltrim($request->getUrl()->path, '/');
+        $contentFile = ContentFiles::findBestFit('develop', $url);
+        if ($contentFile === null) {
+            return $this->handleResource("public", $url, $pageRenderer);
+        }
+
+        return $this->handleContent($contentFile, ['version' => 'develop', $url], $pageRenderer);
+    }
+
+    #[Get('regex:(?<version>\d+\.\d+(?:\.\d+)?|main|develop)(?:\/(?<path>.*))?', 10)]
+    public function handleGetVersionContent(Request $request, array $templateValues, PageRenderer $pageRenderer): ResponseBuilder
+    {
+        $contentFile = ContentFiles::findBestFit($templateValues['version'], $templateValues['path'] ?? '');
+        if ($contentFile === null) {
+            return $this->handleResource("public", (string)$request->getUrl(), $pageRenderer);
+        }
+
+        return $this->handleContent($contentFile, $templateValues, $pageRenderer);
+    }
+
+    #[Get('regex:\/(?<url>vendor\/.*)', 10)]
+    public function handleVendor(string $url, PageRenderer $pageRenderer): ResponseBuilder
+    {
+        return $this->handleResource("", $url, $pageRenderer);
+    }
+
+    private function handleContent(string $contentFile, array $templateValue, PageRenderer $pageRenderer): ResponseBuilder
+    {
+        $body = $pageRenderer->render($contentFile, $templateValue);
+
+        return Response::build()
+            ->responseCode(ResponseCode::OK)
+            ->htmlBody($body)
+        ;
+    }
+
+    private function handleResource(string $parent, string $url, PageRenderer $pageRenderer): ResponseBuilder
+    {
+        $resourcePath = Path::join(__DIR__, "..", $parent, ltrim($url, '/'));
+        if (is_file($resourcePath)) {
+            return $this->wrapResource($resourcePath);
+        }
+
+        return $this->notFound($url, $pageRenderer);
+    }
+
+    private function notFound(string $requestedUrl, PageRenderer $pageRenderer): ResponseBuilder
+    {
+        $notFoundFile = Path::join(__DIR__, "..", "content", "not-found.md");
+        $body = $pageRenderer->render($notFoundFile, ['url' => $requestedUrl, 'title' => 'Not Found']);
+
+        return Response::build()
+            ->responseCode(ResponseCode::NotFound)
+            ->htmlBody($body)
+        ;
+    }
+
+    private function wrapResource(string $path): ResponseBuilder
+    {
+        return Response::build()
+            ->responseCode(ResponseCode::OK)
+            ->fileBody($path, MimeType::fromExtension(pathinfo($path, PATHINFO_EXTENSION)))
+        ;
+    }
+}
