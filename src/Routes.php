ef8aa41a7ae4ba35bb8ba38619ec51e4aa909c03
Ricardo Boss <contact@ricardoboss.de>
2022-04-27 02:54:08 +0200

    Use CustomMimeType::fromFile instead of MimeType::fromExtension for soft error handling
    Also some formatting

--- src/Routes.php
+++ src/Routes.php
@@ -8,7 +8,7 @@ use Elephox\Http\Contract\Request;
 use Elephox\Http\Contract\ResponseBuilder;
 use Elephox\Http\Response;
 use Elephox\Http\ResponseCode;
-use Elephox\Mimey\MimeType;
+use Elephox\Support\CustomMimeType;
 use Elephox\Web\Routing\Attribute\Controller;
 use Elephox\Web\Routing\Attribute\Http\Any;
 use Elephox\Web\Routing\Attribute\Http\Get;
@@ -16,71 +16,64 @@ use Elephox\Web\Routing\Attribute\Http\Get;
 #[Controller('/')]
 class Routes
 {
-    #[Any('regex:(?<url>.*)')]
-    public function handleAny(string $url, PageRenderer $pageRenderer): ResponseBuilder
-    {
-        $url = ltrim($url, '/');
-        $contentFile = ContentFiles::findBestFit('develop', $url);
-        if ($contentFile === null) {
-            return $this->handleResource("public", $url, $pageRenderer);
-        }
+	#[Any('regex:(?<url>.*)')]
+	public function handleAny(string $url, PageRenderer $pageRenderer): ResponseBuilder
+	{
+		$url = ltrim($url, '/');
+		$contentFile = ContentFiles::findBestFit('develop', $url);
+		if ($contentFile === null) {
+			return $this->handleResource("public", $url, $pageRenderer);
+		}
 
-        return $this->handleContent($contentFile, ['version' => 'develop', 'branch' => 'develop', 'path' => $url], $pageRenderer);
-    }
+		return $this->handleContent($contentFile, ['version' => 'develop', 'branch' => 'develop', 'path' => $url], $pageRenderer);
+	}
 
-    #[Get('regex:(?<version>\d+\.\d+(?:\.\d+)?|develop)(?:\/(?<path>.*))?', 10)]
-    public function handleGetVersionContent(Request $request, PageRenderer $pageRenderer, string $version, ?string $path = null): ResponseBuilder
-    {
-        $contentFile = ContentFiles::findBestFit($version, $path ?? '');
-        if ($contentFile === null) {
-            return $this->handleResource("public", (string)$request->getUrl(), $pageRenderer);
-        }
+	#[Get('regex:(?<version>\d+\.\d+(?:\.\d+)?|develop)(?:\/(?<path>.*))?', 10)]
+	public function handleGetVersionContent(Request $request, PageRenderer $pageRenderer, string $version, ?string $path = null): ResponseBuilder
+	{
+		$contentFile = ContentFiles::findBestFit($version, $path ?? '');
+		if ($contentFile === null) {
+			return $this->handleResource("public", (string)$request->getUrl(), $pageRenderer);
+		}
 
-        return $this->handleContent($contentFile, ['version' => $version, 'branch' => $version === 'develop' ? 'develop' : ('release/' . $version), 'path' => $path ?? ''], $pageRenderer);
-    }
+		return $this->handleContent($contentFile, ['version' => $version, 'branch' => $version === 'develop' ? 'develop' : ('release/' . $version), 'path' => $path ?? ''], $pageRenderer);
+	}
 
-    #[Get('regex:(?<url>vendor\/.*)', 10)]
-    public function handleVendor(string $url, PageRenderer $pageRenderer): ResponseBuilder
-    {
-        return $this->handleResource("", $url, $pageRenderer);
-    }
+	#[Get('regex:(?<url>vendor\/.*)', 10)]
+	public function handleVendor(string $url, PageRenderer $pageRenderer): ResponseBuilder
+	{
+		return $this->handleResource("", $url, $pageRenderer);
+	}
 
-    private function handleContent(string $contentFile, array $templateValue, PageRenderer $pageRenderer): ResponseBuilder
-    {
-        $body = $pageRenderer->render($contentFile, $templateValue);
+	private function handleContent(string $contentFile, array $templateValue, PageRenderer $pageRenderer): ResponseBuilder
+	{
+		$body = $pageRenderer->render($contentFile, $templateValue);
 
-        return Response::build()
-            ->responseCode(ResponseCode::OK)
-            ->htmlBody($body)
-        ;
-    }
+		return Response::build()
+			->responseCode(ResponseCode::OK)
+			->htmlBody($body);
+	}
 
-    private function handleResource(string $parent, string $url, PageRenderer $pageRenderer): ResponseBuilder
-    {
-        $resourcePath = Path::join(__DIR__, "..", $parent, ltrim($url, '/'));
-        if (is_file($resourcePath)) {
-            return $this->wrapResource($resourcePath);
-        }
+	private function handleResource(string $parent, string $url, PageRenderer $pageRenderer): ResponseBuilder
+	{
+		$resourcePath = Path::join(__DIR__, "..", $parent, ltrim($url, '/'));
+		if (is_file($resourcePath) && str_starts_with($_SERVER['DOCUMENT_ROOT'], realpath($resourcePath))) {
+			return Response::build()
+				->responseCode(ResponseCode::OK)
+				->fileBody($resourcePath, CustomMimeType::fromFile($resourcePath))
+			;
+		}
 
-        return $this->notFound($url, $pageRenderer);
-    }
+		return $this->notFound($url, $pageRenderer);
+	}
 
-    private function notFound(string $requestedUrl, PageRenderer $pageRenderer): ResponseBuilder
-    {
-        $notFoundFile = Path::join(__DIR__, "..", "content", "not-found.md");
-        $body = $pageRenderer->render($notFoundFile, ['url' => $requestedUrl, 'title' => 'Not Found']);
+	private function notFound(string $requestedUrl, PageRenderer $pageRenderer): ResponseBuilder
+	{
+		$notFoundFile = Path::join(__DIR__, "..", "content", "not-found.md");
+		$body = $pageRenderer->render($notFoundFile, ['url' => $requestedUrl, 'title' => 'Not Found']);
 
-        return Response::build()
-            ->responseCode(ResponseCode::NotFound)
-            ->htmlBody($body)
-        ;
-    }
-
-    private function wrapResource(string $path): ResponseBuilder
-    {
-        return Response::build()
-            ->responseCode(ResponseCode::OK)
-            ->fileBody($path, MimeType::fromExtension(pathinfo($path, PATHINFO_EXTENSION)))
-        ;
-    }
+		return Response::build()
+			->responseCode(ResponseCode::NotFound)
+			->htmlBody($body);
+	}
 }
